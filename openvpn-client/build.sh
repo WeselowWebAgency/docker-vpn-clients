14d57c40b3b43bb90522b9d14a9131e0caceda7b
Александр Веселов <598746@gmail.com>
2022-08-17 01:56:41 +0400

    Update build.sh

--- openvpn-client/build.sh
+++ openvpn-client/build.sh
@@ -2,9 +2,15 @@
 
 echo == Converting files ...
 find . -name '*' | xargs dos2unix
+echo == Converting files ... done!
+echo
 
 echo == Building Docker Image ...
 docker build . -t aweselow/linux-openvpn-client:latest
+echo == Building Docker Image ... done!
+echo
 
 echo == Pushing Docker Image to Hub ...
 docker push aweselow/linux-openvpn-client:latest
+echo == Pushing Docker Image to Hub ... done!
+echo
